3eccd2663119ecd5db10c41b99dfafa2845d7b2a
Alexei Selivanov <alexei@slowmotion.io>
2019-09-06 22:04:09 +0300

    Compensate UIPicker vertical spacing

--- Classes/UIMultiPicker.swift
+++ Classes/UIMultiPicker.swift
@@ -1,15 +1,15 @@
 import UIKit
 
-@IBDesignable 
+@IBDesignable
 open class UIMultiPicker: UIControl {
-    
+
     @objc
     public var options: [String] = [] {
         didSet {
             picker.reloadComponent(0)
         }
     }
-    
+
     @objc
     public var selectedIndexes: [Int] = [] {
         didSet {
@@ -23,76 +23,89 @@ open class UIMultiPicker: UIControl {
             picker.setNeedsLayout()
         }
     }
-    
+
     @objc
     public var textAlign: NSTextAlignment = .center {
         didSet {
             picker.setNeedsLayout()
         }
     }
-    
+
     @IBInspectable
     public var color: UIColor = UIColor.black {
         didSet {
             picker.setNeedsLayout()
         }
     }
-    
+
     @IBInspectable
-    public var rowHeight = 44 {
+    public var rowHeight: CGFloat = 44 {
         didSet {
             picker.setNeedsLayout()
         }
     }
-    
+
     @objc
     public func highlight(_ at: Int, animated: Bool = true) {
         picker.selectRow(at, inComponent: 0, animated: animated)
     }
-    
+
     private let picker = UIMultiPickerView()
-    
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
     }
-    
+
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
     }
-    
+
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        let height = bounds.size.height
+        let h = height - 22 // Vertical padding
+        let offset = max(0, (UIMultiPicker.A * h * h + (UIMultiPicker.B - 1) * h + UIMultiPicker.C) / 2)
+        picker.frame.size.height = height + 2 * offset
+        picker.frame.origin.y = -offset
+    }
+
     fileprivate func setup() {
         picker.parent = self
         addSubview(picker)
-        picker.frame = bounds // Make picker spans parent view
+        picker.frame = bounds
         picker.autoresizingMask = [.flexibleWidth, .flexibleHeight]
     }
+
+    fileprivate static let A: CGFloat = 0.001138663967611336
+    fileprivate static let B: CGFloat = 1.3481781376518218
+    fileprivate static let C: CGFloat = -64.72064777327921
 }
 
 class UIMultiPickerView: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSource
 {
     weak var parent: UIMultiPicker!
     var proxy: TableViewProxy!
-    
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
     }
-    
+
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
     }
-    
+
     func setup() {
         self.delegate = self
         self.dataSource = self
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
-  
+
         layer.borderWidth = 0 // Main view rounded border
 
         // Component borders
@@ -100,7 +113,7 @@ class UIMultiPickerView: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSou
             $0.layer.borderWidth = 0
             $0.isHidden = $0.frame.height <= 1.0
         }
-        
+
         let tables = [
             subviews[0].subviews[0].subviews[0].subviews[0] as! UITableView, // top
             subviews[0].subviews[0].subviews[1].subviews[0] as! UITableView, // bottom
@@ -114,21 +127,21 @@ class UIMultiPickerView: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSou
             }
         }
     }
-    
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return parent.options.count
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return parent.options[row]
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
-        return CGFloat(parent.rowHeight)
+        return parent.rowHeight
     }
 }
 
@@ -136,34 +149,34 @@ class TableViewProxy: NSObject, UITableViewDataSource
 {
     weak var multiPicker: UIMultiPicker!
     let dataSource: UITableViewDataSource
-    
+
     init(dataSource: UITableViewDataSource) {
         self.dataSource = dataSource
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = dataSource.tableView(tableView, cellForRowAt: indexPath)
         let label = cell.subviews[1] as! UILabel
-        
+
         let tap = cell.gestureRecognizers![0] as! UITapGestureRecognizer
         cell.tag = indexPath.row
         tap.addTarget(self, action: #selector(self.handleCellTap))
-        
+
         label.alpha = 1
         label.font = multiPicker.font
         label.textAlignment = multiPicker.textAlign
         label.textColor = multiPicker.selectedIndexes.contains(indexPath.row) ?
             multiPicker.tintColor :
             multiPicker.color
-        
+
         return cell
     }
-    
-    @objc 
+
+    @objc
     func handleCellTap(sender: UITapGestureRecognizer) {
         let cell = sender.view as! UITableViewCell
         let row = cell.tag
-        
+
         if (multiPicker.selectedIndexes.contains(row)) {
             multiPicker.selectedIndexes = multiPicker.selectedIndexes.filter { $0 != row }
         } else {
@@ -171,7 +184,7 @@ class TableViewProxy: NSObject, UITableViewDataSource
         }
         multiPicker.sendActions(for: .valueChanged)
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataSource.tableView(tableView, numberOfRowsInSection: section)
     }
